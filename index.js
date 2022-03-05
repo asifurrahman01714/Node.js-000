9b3115312e8ce8e8afb3df2f36bc7bd75295d669
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-03-05 14:05:28 +0600

    Creating get api

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/', (req, res) => {
   res.send(product);
 })
 
+// creating another API
+app.get('/api/v1/products', (req, res) => {
+    const products = [
+        {
+            "id": 1,
+            "name": "product 1",
+            "price": "10",
+        },
+        {
+            "id": 2,
+            "name": "product 2",
+            "price": "20",
+        },
+    ]
+    res.send(products);
+});
+
+
 app.listen(port, () => {
   console.log(`Example listening on port ${port}`)
 })
